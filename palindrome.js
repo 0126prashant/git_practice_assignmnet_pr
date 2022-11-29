59d68d98846da57727d7ca819795c44d201a4822
Sankalp S <sankalp.sas10@gmail.com>
2022-11-29 23:45:20 +0530

    assignment_completed

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,12 @@
-let str = "madam"
+let str = "madam";
 
-   let bag = ""
+   let bag = "";
+
 for(let i=str.length-1 ; i>=0 ; i--){
-    bag += str[i]
+    bag = str[i]+bag;
   }
+if(bag==str){
+console.log("It is a Palindrome");
+}else{
+console.log("It is not a Palindrome");
+}
--- prime.js
+++ prime.js
@@ -1,7 +1,14 @@
 let n=13;
-
+let count=0;
 
 for(let i=1 ; i<=n ; i++){
-    if(n%i ==0){
-     count++
+    if(n%i==0){
+     count++;
   }
+}
+
+if(count<=2){
+console.log("It is a Prime Number");
+}else{
+console.log("It is not a Prime Number");
+}

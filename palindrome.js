9a9f2986c990c52afa374780de9e5ebb78b0ddf5
Prashant <prashantsom75@gmail.com>
2022-11-29 23:06:43 +0530

    created_Prime_and_Prlindrome_problem

---
+++ palindrome.js
@@ -0,0 +1,6 @@
+let str = "madam"
+
+   let bag = ""
+for(let i=str.length-1 ; i>=0 ; i--){
+    bag += str[i]
+  }
---
+++ prime.js
@@ -0,0 +1,7 @@
+let n=13;
+
+
+for(let i=1 ; i<=n ; i++){
+    if(n%i ==0){
+     count++
+  }
